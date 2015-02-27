3fc09311e1864a908672578986bbced21e69754c
rmcgr1 <rmcgr1@umbc.edu>
2015-02-27 17:07:07 -0500

    updated build script

--- test.sh
+++ test.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
 
-	#!/bin/bash
-
+mkdir -p $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/temp
 
 cp $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/client.go $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/client_p1_rmcgr1
 cd $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/client_p1_rmcgr1
 go install
+
 cp $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/server.go $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/server_p1_rmcgr1
 cd $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/server_p1_rmcgr1
 go install
-cd $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/
-rm ./submission_rmcgr1.tar
-cp $GOPATH/bin/server_p1_rmcgr1 $GOPATH/bin/client_p1_rmcgr1 .
+
+cd $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/temp
+rm $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/temp/submission_rmcgr1.tar
+
+cp $GOPATH/bin/server_p1_rmcgr1 $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/temp
+cp $GOPATH/bin/client_p1_rmcgr1 $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/temp
+cp $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/config.txt $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/temp
+
+
 tar -czf submission_rmcgr1.tar client_p1_rmcgr1 server_p1_rmcgr1 config.txt
-rm ./client_p1_rmcgr1 ./server_p1_rmcgr1
 scp $GOPATH/src/github.com/rmcgr1/JSON-Client-Server/submission_rmcgr1.tar user@192.168.2.112:~
 ssh user@192.168.2.112 'tar -xzf ~/submission_rmcgr1.tar'
